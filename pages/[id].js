f97b15319af03288a96889707e8e9e9379431d58
Lee Wan Geun <wglee21g@gmail.com>
2022-03-02 15:52:02 +0000

    renewal init

--- pages/[id].js
+++ pages/[id].js
@@ -142,13 +142,13 @@ export default function Post({ page, blocks }) {
   return (
     <div>
       <Head>
-        <title>{page.properties.Name.title[0].plain_text}</title>
+        <title>{page.properties.Page.title[0].plain_text}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <article className={styles.container}>
         <h1 className={styles.name}>
-          <Text text={page.properties.Name.title} />
+          <Text text={page.properties.Page.title} />
         </h1>
         <section>
           {blocks.map((block) => (
